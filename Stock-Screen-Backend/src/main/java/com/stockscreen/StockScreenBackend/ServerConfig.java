ec7881b31f809b940911869e5a10dd4e960625fe
270004270young1 <youngminghei@gmail.com>
2023-09-02 11:02:17 +0800

    Add cors allow

--- Stock-Screen-Backend/src/main/java/com/stockscreen/StockScreenBackend/ServerConfig.java
+++ Stock-Screen-Backend/src/main/java/com/stockscreen/StockScreenBackend/ServerConfig.java
@@ -3,6 +3,8 @@ package com.stockscreen.StockScreenBackend;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ServerConfig {
@@ -10,5 +12,14 @@ public class ServerConfig {
     @Bean
     public RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/**").allowedOrigins("http://127.0.0.1:5173/");
+            }
+        };
     }
 }
